85c81258ed8040f33348f75a9dd18579acf301c5
Mikolaj Wojtowicz <70533837+not-pancakes@users.noreply.github.com>
2022-09-12 08:20:45 +0200

    Update index.js

--- src/pages/pensieve/index.js
+++ src/pages/pensieve/index.js
@@ -123,8 +123,8 @@ const PensievePage = ({ location, data }) => {
   return (
     <Layout location={location}>
       <Helmet>
-        <title>Pensieve | Chandrika Deb</title>
-        <link rel="canonical" href="https://chandrikadeb7.github.io/pensieve" />
+        <title>Pensieve | Mikolaj Wojtowicz</title>
+        <link rel="canonical" href="https://mwojtowicz.dev/pensieve" />
       </Helmet>
 
       <StyledMainContainer>
