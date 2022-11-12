02130d91d27a688e66e2902641bf3a2664ff3d33
Mikolaj Wojtowicz <70533837+not-pancakes@users.noreply.github.com>
2022-11-12 15:02:29 +0100

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -59,29 +59,6 @@ const StyledGitHubInfo = styled.div`
     margin-right: 5px;
   }
 `;
-
-const Footer = () => {
-  const [githubInfo, setGitHubInfo] = useState({
-    stars: null,
-    forks: null,
-  });
-
-  useEffect(() => {
-    if (process.env.NODE_ENV !== 'production') {
-      return;
-    }
-    fetch('https://api.github.com/repos/bchiang7/v4')
-      .then(response => response.json())
-      .then(json => {
-        const { stargazers_count, forks_count } = json;
-        setGitHubInfo({
-          stars: stargazers_count,
-          forks: forks_count,
-        });
-      })
-      .catch(e => console.error(e));
-  }, []);
-
   return (
     <StyledContainer>
       <StyledSocial>
